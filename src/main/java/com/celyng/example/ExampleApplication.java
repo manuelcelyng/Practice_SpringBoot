f37549e0f782eb3aea0aa1cfca31c47194819a26
manuelcely <fernandocly18@gmail.com>
2024-10-02 19:02:51 -0500

    Using Spring Rest and understand diferent things

--- src/main/java/com/celyng/example/ExampleApplication.java
+++ src/main/java/com/celyng/example/ExampleApplication.java
@@ -10,16 +10,7 @@ import java.util.Collections;
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(ExampleApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-
-		var ctx = app.run(args);
-
-		MyFirstService myFirstService =  ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getNameProperty());
-		System.out.println(myFirstService.getNumberProperty());
-
+		SpringApplication.run(ExampleApplication.class);
 	}
 
 
